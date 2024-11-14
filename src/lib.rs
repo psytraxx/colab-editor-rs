3bdaf977401163449c5ce4bb6638ee4f3a2c27af
eric <psytraxx@gmail.com>
2024-11-14 23:01:35 +0100

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,25 @@
 #![recursion_limit = "128000"]
-
-use chat::{chat_model::ChatModel, web_rtc_manager::WebRTCManager};
+mod chat_component;
+mod chat_service;
+use chat_component::ChatComponent;
+use chat_service::WebRTCChatService;
 use wasm_bindgen::prelude::*;
+use yew::{function_component, html, Html};
 
 mod chat;
+
+#[function_component(App)]
+fn app() -> Html {
+    let joke_service = WebRTCChatService::new("stun:stun.l.google.com:19302");
+
+    html! {
+        <ChatComponent<WebRTCChatService> service={joke_service} />
+    }
+}
 
 // Called when the wasm module is instantiated
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
-    yew::Renderer::<ChatModel<WebRTCManager>>::new().render();
+    yew::Renderer::<App>::new().render();
     Ok(())
 }
